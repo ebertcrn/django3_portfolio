a17e541abeb3e312474d3be0b4424c89307fa2e4
Ebert <ebertcrn@gmail.com>
2020-09-01 18:41:20 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(r*%swz=^&8*yipx50*n=6811x8y!5b35gc#b&a==e(-g&p*f_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ebertcrn.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # save images in this folder
 
 MEDIA_URL = '/media/'   # can be any name here! Ex: '/coolpics/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # save images in this folder
